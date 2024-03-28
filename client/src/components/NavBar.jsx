7ed7c6b9227e07e00d0b0192101fd4a59101d298
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-03-29 00:55:46 +0530

    worked on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
     return (
-        <header className="home-header">
-            <div className="home-link">
-                <img src="/src/assets/logo.png" alt="" width="50px"/>
-            </div>
-            <div className="dashboard-links menu-links">
+        <header className="dashboard-header">
+            <Link className="home-link" to="/dashboard">
+                <img src="/src/assets/logo.png" alt="" width="40px"/><p> SmartGrid</p>
+            </Link>
+            <div className="dashboard-links">
                 <ul className="menu-items">
-                    <li>Dashboard</li>
-                    <li>Monitor</li>
-                    <li>Workflows</li>
+                    <li><img src="/src/assets/dashboard-icon.svg" alt="" /> Dashboard</li>
+                    <li><img src="/src/assets/monitor.svg" alt="" /> Monitor</li>
+                    <li><img src="/src/assets/workflow.svg" alt="" /> Workflows</li>
+                    <li><img src="/src/assets/settings.svg" alt="" /> Settings</li>
                 </ul>
             </div>
-            <div className="header-buttons">
+            <div className="dashboard-header-buttons">
+                <button className="dashboard-notification">
+                    <img src="/src/assets/bell.svg" alt="" />
+                </button>
                 <button>
                     <img src="/src/assets/sun.svg" alt="Theme Button" />
                 </button>
